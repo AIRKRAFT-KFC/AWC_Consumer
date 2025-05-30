fad11e734788158c60341d3845cf86b04f7a271c
di0308 <di0308@naver.com>
2025-05-30 22:00:21 +0900

    fix: cors

--- src/main/java/com/example/awc_consumer/config/WebSocketConfig.java
+++ src/main/java/com/example/awc_consumer/config/WebSocketConfig.java
@@ -18,6 +18,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-AWC").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws-AWC")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
     }
 }
