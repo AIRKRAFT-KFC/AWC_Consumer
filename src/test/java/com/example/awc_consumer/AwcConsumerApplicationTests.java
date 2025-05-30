004f0c0b28f7b367d3ba2c7385963a5787f3d853
di0308 <di0308@naver.com>
2025-05-30 21:12:02 +0900

    first commit

---
+++ src/test/java/com/example/awc_consumer/AwcConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awc_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwcConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
